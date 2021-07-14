aff47d801f41b2693021cc999529da5641fdab70
fn-ysahin <ysahin895@gmail.com>
2021-07-14 15:12:55 +0300

    modifed

--- cassandra/models.py
+++ cassandra/models.py
@@ -5,14 +5,53 @@ import subprocess
 import pprint
 
 def cassandra_connect(shell):
-    
     output = os.popen(shell).read()
     print(output)
     print (type(output))
-    return output
-
+    x =  output.splitlines()
+    return x
 
 def connect_status():
     cmd = cassandra_connect("nodetool status")
+    print("%s", str(cmd))
+    return cmd
+
+#This command retrieves a list of active and pending tasks
+def  connect_tpstats():
+    cmd = cassandra_connect("nodetool tpstats")
+    print("%s", str(cmd))
+    return cmd
+
+#This command verifies if Cassandra is processing compactions fast enough
+def  connect_compactions():
+    cmd = cassandra_connect("nodetool compactionstats")
+    print("%s", str(cmd))
+    return cmd
+
+#This command identifies the tables in which the number of SSTables is growing and 
+#shows disk latencies and number of tombstones read per query
+def  connect_tablestats():
+    cmd = cassandra_connect("nodetool tablestats")
+    print("%s", str(cmd))
+    return cmd
+
+#This command provides further information about tables with high latencies
+def  connect_histogram():
+    cmd = cassandra_connect("nodetool cfhistograms")
+    print("%s", str(cmd))
+    return cmd
+
+def  connect_gospinfo():
+    cmd = cassandra_connect("nodetool gossipinfo")
+    print("%s", str(cmd))
+    return cmd
+
+def  connect_ring():
+    cmd = cassandra_connect("nodetool ring")
+    print("%s", str(cmd))
+    return cmd
+
+def  connect_info():
+    cmd = cassandra_connect("nodetool info")
     print("%s", str(cmd))
     return cmd
--- cassandra/views.py
+++ cassandra/views.py
@@ -1,13 +1,15 @@
-from .models import connect_status
+from .models import connect_compactions, connect_gospinfo, connect_histogram, connect_info, connect_ring, connect_status, connect_tablestats, connect_tpstats
 from django.shortcuts import render
 from django.http import Http404
 
 
 def cassandra_index (request):
-    client_cassandra = connect_status()
-
+    client_cassandra = connect_gospinfo()
+    client_ping =  connect_ring()
+    client_info = connect_info()
     context = {
-
+    "client_info" : client_info,
+    "client_ping" : client_ping,
     "client_cassandra" : client_cassandra
 
     }

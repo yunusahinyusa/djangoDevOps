78d84c3a8002e9c4261d953ce371c502b17e6787
fn-ysahin <ysahin895@gmail.com>
2021-07-16 16:03:39 +0300

    redis-updated

--- redisclient/models.py
+++ redisclient/models.py
@@ -1,3 +1,4 @@
+from typing import List
 from django import db
 from django.db import connection, models
 import redis
@@ -15,9 +16,16 @@ def redis_connect(result) :
         #ADD THE CONFIG FILE PARAMETER
         r = redis.StrictRedis(**redis_pool)
         data = r.execute_command(result)
-        print("Query Answer:") 
-        pprint.pprint(data)
-        print (type(data))
+        if type(data) == bytes :
+            m = data.decode('UTF-8')
+            m = data.splitlines()
+            print(type(m))
+            return m
+        elif type(data) == List :
+            print("Query Answer:") 
+            pprint.pprint(data)
+            print (type(data))
+        print(type(data))
         return data
 
 
@@ -37,3 +45,78 @@ def get_memory_stats():
     result = redis_connect('memory stats')
     print('%s' , str(result))
     return result
+
+#General information about the Redis server
+def get_info_server():
+    result = redis_connect('info server')
+    print("%s", str(result))
+    return result
+
+#Master/replica replication information
+def get_info_replication():
+    result = redis_connect('info replication')
+    print('%s', str(result))
+    return result
+
+#CPU consumption statistics
+def get_info_cpu():
+    result = redis_connect('info cpu')
+    print('%s', str(result))
+    return result
+
+#Redis command statistics
+def get_info_commandstats():
+    result = redis_connect('info commandstats')
+    print('%s', str(result))
+    return result
+
+#Client connections section
+def get_info_clients():
+    result = redis_connect('info clients')
+    print("%s" ,  str(result))
+    return result
+
+#General statistics
+def get_info_stats():
+    result = redis_connect('info stats')
+    print('%s' , str(result))
+    return result
+
+#RDB and AOF related information
+def get_info_persistence():
+    result = redis_connect('info persistence')
+    print("%s" ,  str(result))
+    return result
+
+
+#This command provides an internal statistics report from the memory allocator. 
+def get_memory_malloc():
+    result = redis_connect('memory malloc-stats')
+    print('%s' , str(result))
+    return result
+
+#You can collect all 
+#memory utilization metrics data for a Redis instance by running “info memory”.
+def get_memory():
+    result = redis_connect('info memory')
+    print('%s' , str(result))
+    return result
+
+#This makes the slow log remarkably 
+#fast at the point that you can enable the logging of all the commands 
+def get_slowlog():
+    result = redis_connect('slowlog get 2')
+    print('%s', str(result))
+    return result
+
+#The LATENCY HISTORY command returns the raw data of the event 's latency spikes time series.
+def get_latency():
+    result = redis_connect('latency history command')
+    print('%s' , str(result))
+    return result
+
+def get_client_output():
+    result = redis_connect('CLIENT LIST')
+    print('%s' , str(result))
+    return result
+
--- redisclient/views.py
+++ redisclient/views.py
@@ -1,16 +1,26 @@
 from django.http import Http404
 from django.shortcuts import render
-from  .models import get_all_infos, get_memory_stats 
+from  .models import get_all_infos, get_client_output, get_info_clients, get_info_commandstats, get_info_replication, get_info_server, get_info_stats, get_memory, get_memory_stats 
 
 def redis_index (request):
-    client_redis = get_all_infos()
-    client_memory = get_memory_stats()
+    client_commands =  get_info_commandstats()
+    client_output   = get_client_output() 
+    client_info = get_info_clients()   
+    client_replication = get_info_replication()
+    client_memory = get_memory()
+    client_stats  = get_info_stats()
+    
+    
                    
 
     context = {
 
-    "client_redis" : client_redis,
-    "client_memory" :client_memory
+    "client_commands" : client_commands,
+    "client_output" : client_output,
+    "client_info" : client_info,
+    "client_replication" : client_replication,
+    "client_memory" : client_memory,
+    "client_stats" : client_stats
 
     }
 

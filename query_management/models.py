a94abe9c57dab67da67e796f53583640ec398d47
fn-ysahin <ysahin895@gmail.com>
2021-07-26 15:11:48 +0300

    adding tables

--- query_management/models.py
+++ query_management/models.py
@@ -5,6 +5,7 @@ import pprint
 from django.db import connection
 import redis
 import config
+import re
 
 
 def psql_connect(query):
@@ -17,6 +18,7 @@ def psql_connect(query):
         db_query =  cursor.fetchall()
         pprint.pprint(db_query)
         print (type(db_query))
+        #db_query = re.sub(r'[^a-zA-Z0-9]','', db_query)
         return db_query
         
     except (Exception, psycopg2.DatabaseError) as error:
@@ -42,7 +44,10 @@ def procces_execute() :
 def database_conflicts() :
     response = psql_connect('SELECT * FROM pg_stat_database_conflicts')
     print("%s", str(response))
-    return response
+    result = []
+    for r in response:
+        result.append({"datid": r[0], "datname": r[1], "confl_space": r[2], "conf_lock":r[3], "conf_snapshot":r[4],"conf_buffer":r[5],"conf_deadlock":r[6]})
+    return result
 
 # number of connections
 def connections_number() :
@@ -54,13 +59,19 @@ def connections_number() :
 def disk_usage() :
     response = psql_connect("SELECT pg_size_pretty(pg_database_size('postgres')) AS mydbsize")
     print("%s", str(response))
-    return response
+    result = []
+    for r in response:
+        result.append({"usage_disk": r[0]})
+    return result
 
 #Showing unique number of active users
 def active_user() :
     response = psql_connect('SELECT datname, usename , COUNT(*) FROM pg_stat_activity GROUP BY datname, usename')
     print("%s", str(response))
-    return response
+    result = []
+    for r in response:
+        result.append({"base_name": r[0], "us_name": r[1], "count": r[2]})
+    return result
 
 #Displays statistics for each of the user tables.
 def user_statics() :
@@ -98,7 +109,10 @@ def count_connection() :
 def database_logs() : 
     response = psql_connect('SELECT locktype, database, relation::regclass, mode, pid FROM pg_locks')
     print("%s", str(response))
-    return response
+    result = []
+    for r in response:
+        result.append({"lock_type": r[0], "database_result" : r[1],"relation_name":r[2],"mode_name":r[3],"pid":r[4]})
+    return result
 
 
 #PG_STAT_ALLTABLES
@@ -257,13 +271,19 @@ def stat_replication() :
     response = psql_connect('SELECT EXTRACT(EPOCH FROM (now() - pg_last_xact_replay_timestamp()))::INT')
     #response = psql_connect('SELECT * FROM pg_stat_replication')
     print("%s", str(response))
-    return response
+    result = []
+    for r in response:
+        result.append({"stat_replication": r[0]})
+    return result
 
 #Connection per database
 def connection_number() :
     response = psql_connect('SELECT sum(numbackends) FROM pg_stat_database')
     print("%s", str(response))
-    return response
+    result = []
+    for r in response:
+        result.append({"connumber": r[0],})
+    return result
 
 #temp_bytes
 def temp_bytes() :
@@ -276,7 +296,10 @@ def sum_deadlocks() :
 
     response = psql_connect('SELECT sum(deadlocks) FROM pg_stat_database')
     print("%s", str(response))
-    return response
+    result = []
+    for r in response:
+        result.append({"sum_deadlock": r[0]})
+    return result
 
 #Number of live rows fetched by sequential scans
 def seq_tup_read() :
@@ -287,17 +310,27 @@ def seq_tup_read() :
 def tables_disk_usage():
     response = psql_connect("SELECT nspname || '.' || relname AS relation, pg_size_pretty(pg_total_relation_size(C.oid)) AS total_size FROM pg_class C LEFT JOIN pg_namespace N ON (N.oid = C.relnamespace) WHERE nspname NOT IN ('pg_catalog', 'information_schema') AND C.relkind <> 'i' AND nspname !~ '^pg_toast' ORDER BY pg_total_relation_size(C.oid) DESC LIMIT 5")
     print("%s", str(response))
-    return response
+    result = []
+    for r in response:
+        result.append({"table_name": r[0], "disk_usage": r[1]})
+    return result
 
 def tables_rows():
     response = psql_connect("SELECT schemaname,relname,n_live_tup FROM pg_stat_user_tables ORDER BY n_live_tup DESC LIMIT 5")
     print("%s",str(response))
-    return response
+    result = []
+    for r in response:
+        result.append({"relname_table": r[1] , "schema_name" : r[0]})
+    return result
+
 
 def dead_tuples():
     response = psql_connect('SELECT relname, n_dead_tup FROM pg_stat_user_tables LIMIT 5')
     print("%s",str(response))
-    return response
+    result = []
+    for r in response:
+        result.append({"relaname": r[0], "total": r[1]})
+    return result
 
 
 
